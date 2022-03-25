4abc54f3efe1a5f6cb8ab1b113c55b28f285b001
emelieloof <73931239+emelieloof@users.noreply.github.com>
2022-03-25 14:18:34 +0100

    Update KL-UCB_new.py
    Added comments

--- KL-UCB_new.py
+++ KL-UCB_new.py
@@ -61,47 +61,58 @@ class KLUCB:
                 curr.append(self.rewards[i][j] / self.counts[i][j])
             means.append(curr)
         return means
+    
+    def get_bounds(self, means, t): #Function to get the Q values of each arm
+        if not self.clustering: # This is used for ordinary KL-UCB
+            Q = []
+            for i in range(len(means)):
+                Q.append([0] * len(means[i]))
 
-    def get_bounds(self, means, t): # this dunction caculates the q-values for each arm
-        Q = []
-        for i in range(len(means)):
-            Q.append([0] * len(means[i]))
+            for i in range(len(means)):
+                for j in range(len(means[i])):
+                    if means[i][j] == 1:
+                        Q[i][j] = 1
+                    else:
+                        q = np.linspace(means[i][j], 1, 50)
+                        low = 0
+                        high = len(q) - 1
+                        while high > low:
+                            mid = round((low + high) / 2)
+                            prod = np.sum((self.counts[i][j]) * (fun.kl(means[i][j], q[mid])))
+                            if prod < np.log(t):
+                                low = mid + 1
+                            else:
+                                high = mid - 1
+                        Q[i][j] = q[low]
+            return Q
 
-        epsilon = 0.001
-        for i in range(len(means)):
-            for j in range(len(means[i])):
-                if means[i][j] == 1: # This condition is nessasary since the interval in which we search the q-value can't be empty
-                    Q[i][j] = 1
-                else: # find the optimal q with binary search
-                    q = np.linspace(means[i][j], 1, 50) 
-                    low = 0
-                    high = len(q) - 1
-                    while high > low:
-                        mid = round((low + high) / 2)
-                        prod = np.sum((self.counts[i][j]) * (fun.kl(means[i][j], q[mid])))
-                        if prod < np.log(t + np.log(t)):
-                            low = mid + 1
-                        else:
-                            high = mid - 1
-                    Q[i][j] = q[low]
+        else: # Here's for our algo
+            Q = []
+            max_mean_index = self.get_max_index(means, False, 0)
+            for i in range(len(means)):
+                Q.append([0] * len(means[i]))
 
-        if self.clustering: # If we uant to use our algorithm we want to see if the additional conditions hold
-            max_mean_index = self.get_max_index(means)
-            max_q_index = self.get_max_index(Q)
-            if max_mean_index[0] == max_q_index[0]: # If the max q-value belongs to the current optimal cluster we donät need to check additonal conditions
-                return Q
-            else:
-                 for i in range(len(means)):# looping through the clusters
-                    if not i == max_mean_index[0] and np.any(Q[i] >= np.max(Q[max_mean_index[0]])):# checking if we're in the optimal cluster and if any of the q-values are greater than the q- value of the optimal arm
-                        for j in range(len(means[i])): #looping through the cluster
-                            if Q[i][j] >= np.max(Q[max_mean_index[0]]): #checking if the q-value of the current arm is greater than the q-value of the optimal arm 
-                                limit = self.get_limit([i, j], max_mean_index, Q, means) # calculating the penalty term
-                                if not limit < np.log(t) + np.log(np.log(t)): #checking if condition holds
-                                    Q[i][j] = min(means[max_mean_index[0]]) # if it doesn't hold lower the q-value so it doesn't get 
-                return Q
-        return Q
-
-
+            for i in range(len(means)):
+                for j in range(len(means[i])):
+                    if means[i][j] == 1:
+                        Q[i][j] = 1
+                    else:
+                        q = np.linspace(means[i][j], 1, 50)
+                        low = 0
+                        high = len(q) - 1
+                        while high > low:
+                            mid = round((low + high) / 2)
+                            if not i == max_mean_index[0]: #Calculating penalty term if we're not in the optimal cluster
+                                limit = self.get_limit([i, j], max_mean_index, q[mid], means)
+                            else: #Same as ordinary KL-UCB if we're in the optimal cluster
+                                limit = self.counts[i][j] * fun.kl(means[i][j], q[mid])
+                            if limit < np.log(t):
+                                low = mid + 1
+                            else:
+                                high = mid - 1
+                        Q[i][j] = q[low]
+            return Q
+    
     def get_kl(self, i, j, means, max_cluster): # function to get the sum of the kl-terms used in the penalty term
         sum = 0
         for k in range(len(self.counts[i])):
@@ -115,16 +126,17 @@ class KLUCB:
                      fun.kl(means[max_q_index[0]][max_q_index[1]], Q[max_q_index[0]][max_q_index[1]])
         return kl_sum + max_q_prod
 
-        def get_max_index(self, l, isq, optimal_cluster): # function to get max index of uneven list
-        if isq: 
-            for i in range(len(l)):
-                if not i == optimal_cluster: # Want to make sure we get the optimal cluster as optimal
-                    for j in range(len(l[i])):
-                        l[i][j] == 0
+    def get_max_index(self, l, isq, optimal_cluster): # function to get max index of uneven list
         new_list = self.reshape(l)
+        max = np.max(new_list)
+        if isq: #If the optimal cluster is optimal we want to return it (this prevents from returning just the first optimal cluster if several are equal)
+            for i in range(len(l)):
+                for j in range(len(l[i])):
+                    if l[i][j] == max and i == optimal_cluster:
+                        return i, j
         max_index = np.argmax(new_list)
-
         return self.cluster_belongings[max_index], self.arm_belongings[max_index]
+
     def reshape(self, list):# Function to reshape an uneven list
         new_list = np.zeros(len(self.arm_belongings))
         index = 0
